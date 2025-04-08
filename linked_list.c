dd2b485d488bf1f63707e69caf238b79461e7204
Aliya Carmon <carmon0@purdue.edu>
2025-04-08 18:13:32 -0400

    insertattail

--- linked_list.c
+++ linked_list.c
@@ -23,15 +23,33 @@ void insert_at_tail(struct linked_list *list, size_t value) {
 }
 
 size_t remove_from_head(struct linked_list *list) { 
-  
-  return 0; }
+  if (list -> head == NULL)
+  {
+    return 0;
+  }
+  list -> head = list -> head -> next 
+  return 0; 
+}
 
 size_t remove_from_tail(struct linked_list *list) { 
-  
-  return 0; }
+  linked_list * list2 = list;
+  while(list2 -> head -> next != NULL)
+  {
+    list2 -> head = list2 -> head -> next;
+  }
+  list2 -> head = NULL;
+  //how do i delete the last node without losing the head?
+  return 0; 
+}
 
 void free_list(struct linked_list list) {
-  
+  linked_list list2 = list
+  while(list -> head != NULL)
+  {
+    list2 -> head = list2 -> head -> next;
+    free(list -> head);
+    list = list2;
+  } 
 }
 
 // Utility function to help you debugging, do not modify
