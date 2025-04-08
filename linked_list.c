55ea24cf2993e1d66b32d5cc10ee459578890403
Aliya Carmon <carmon0@purdue.edu>
2025-04-08 15:27:20 -0400

    modifying

--- linked_list.c
+++ linked_list.c
@@ -2,7 +2,9 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) { 
+  
+  return NULL; }
 
 void insert_at_head(struct linked_list *list, size_t value) {
   
@@ -12,13 +14,21 @@ void insert_at_head(struct linked_list *list, size_t value) {
   ptr -> next = head;
 }
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) {
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) { 
+  
+  return 0; }
 
-void free_list(struct linked_list list) {}
+size_t remove_from_tail(struct linked_list *list) { 
+  
+  return 0; }
+
+void free_list(struct linked_list list) {
+  
+}
 
 // Utility function to help you debugging, do not modify
 void dump_list(FILE *fp, struct linked_list list) {
